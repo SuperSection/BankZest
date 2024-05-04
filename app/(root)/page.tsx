c8f13c124c3be0e4cf71cb8affbdba40bf2c85e1
SuperSection <soumosarkar297@gmail.com>
2024-05-04 16:23:43 +0530

    add right sidebar in home page, complete layout UI and routing pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,27 +1,46 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 
+
 const Home = () => {
-  const loggedIn = { firstName: "Super" };
+  const loggedIn = { firstName: "Super", lastName: "Section", email: "user@example.com" };
 
-  return <section className="home">
-    <div className="home-content">
-      <header className="home-header">
-        <HeaderBox
-          type="greeting"
-          title="Welcome"
-          user={loggedIn?.firstName || "Guest"}
-          subtext="Access and manage your account and transactions efficiently."
-        />
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
 
-        <TotalBalanceBox
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={1250.57}
-        />
-      </header>
-    </div>
-  </section>;
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.57}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[
+          {
+            currentBalance: 112.5,
+          },
+          {
+            currentBalance: 300.72,
+          },
+        ]}
+      />
+    </section>
+  );
 };
+
 
 export default Home;
